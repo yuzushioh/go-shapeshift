d596a83a8ad72ac2ced13d9754234762e2038174
yuzushioh <yuzushioh@gmail.com>
2018-05-12 02:03:04 +0900

    define model in a different file

---
+++ currency_pair.go
@@ -0,0 +1,6 @@
+package shapeshift
+
+// CurrencyPair represents a model for currency pair
+type CurrencyPair struct {
+	Name string `json:"pair,omitempty"`
+}
--- get_api.go
+++ get_api.go
@@ -11,29 +11,18 @@ import (
 
 // GetAPIClient is an interface for a get method api.
 type GetAPIClient interface {
-	GetRate(ctx context.Context, pair *CurrencyPair) (*RateResponse, error)
-}
-
-// CurrencyPair represents a model for currency pair
-type CurrencyPair struct {
-	Name string `json:"pair,omitempty"`
-}
-
-// RateResponse represents a model for rate api response
-type RateResponse struct {
-	Pair string `json:"pair,omitempty"`
-	Rate string `json:"rate"`
+	GetRate(ctx context.Context, pair *CurrencyPair) (*Rate, error)
 }
 
 // GetRate fetches currenct rate for specified currency pair
-func (c *client) GetRate(ctx context.Context, pair *CurrencyPair) (*RateResponse, error) {
+func (c *client) GetRate(ctx context.Context, pair *CurrencyPair) (*Rate, error) {
 	url := fmt.Sprintf("rate/%s", pair.Name)
 	res, err := c.do(ctx, http.MethodGet, url, nil)
 	if err != nil {
 		return nil, err
 	}
 
-	rateResp := new(RateResponse)
+	rateResp := new(Rate)
 	if err := json.Unmarshal(res, rateResp); err != nil {
 		return nil, errors.Wrap(err, "")
 	}
